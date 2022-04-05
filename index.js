1d22413f0db47bf075e10470c8dc249dd33a9681
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-05 14:27:44 -0400

    added test data

--- index.js
+++ index.js
@@ -23,3 +23,15 @@ class Stack {
 }
 
 const stack = new Stack();
+
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push('llama');
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
