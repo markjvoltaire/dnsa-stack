ab09c8d0c91b30bf191c331f16f51a8bbeec4d5b
Mark Voltaire <markjvoltaire@gmail.com>
2022-04-05 14:24:57 -0400

    added varible

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ class Stack {
   constructor() {
     this.stack = [];
   }
+
+  get length() {
+    return this.stack.length;
+  }
+
+  push(item) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.length - 1];
+  }
 }
+
+const stack = new Stack();
